67555a7269d0b534c07909e0f064f3c262c236ae
wayn3r <w9delacruz@gmail.com>
2022-01-05 17:55:23 -0400

    upd: simplificando index

--- src/index.php
+++ src/index.php
@@ -1,28 +1,6 @@
 <?php
-
-/*******************************
- * CONFIGURACIÓN DEL PROYECTO  
- *******************************/
-setlocale(
-    LC_ALL,
-    'Spanish_Dominican_Republic',
-    'Spanish_Spain',
-    'es_ES',
-    'Spanish',
-    'es_ES@euro',
-    'es_ES',
-    'esp'
-);
-
 // Cargando todos los archivos para procesar la petición
 require_once  $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
-
-// configuracion del header, más que nada para petición de CORS
-header("Access-Control-Allow-Origin: http://localhost:3000");
-header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Allow-Headers: *");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-
 $userRouter = require_once('user/router/router.php');
 // Procesando la peticion y devolviendo la respuesta
 $app = \Http\Server::getServer();
